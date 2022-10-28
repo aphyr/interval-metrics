b97256a802ac9a5d5a346189a021711f1b638629
Aphyr <aphyr@aphyr.com>
2022-10-28 11:25:44 -0400

    Version 1.0.1

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject interval-metrics "1.0.1-SNAPSHOT"
+(defproject interval-metrics "1.0.1"
   :description "Time-windowed metric collection objects."
   :dependencies []
   :java-source-paths ["src/interval_metrics"]
-  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :javac-options ["-target" "11" "-source" "11"]
 ;  :warn-on-reflection true
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [criterium "0.4.1"]
-                                  [midje "1.5.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
+                                  [criterium "0.4.6"]
+                                  [midje "1.10.6"]]
+                   :plugins [[lein-midje "3.2.1"]]}})
